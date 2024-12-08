7de6b35ad1bb296796dc4bdc35f2d7c807506c7c
Vladimir Nagibin <vlnagibin@yandex.ru>
2024-12-09 01:32:57 +0700

    Upd admin warehouse

--- backend/warehouses/admin.py
+++ backend/warehouses/admin.py
@@ -22,19 +22,20 @@ class PayFormInline(admin.TabularInline):
 @admin.register(Outlet)
 class OutletAdmin(admin.ModelAdmin):
     inlines = (DeliveryDateInline, PayFormInline, OperationOutletInline)
-    list_display = ('outletExternalCode', 'outletName', 'status', 'warehouse')
+    list_display = ('outletExternalCode', 'outletName', 'status', 'code_B24',
+                    'warehouse')
     list_editable = ('outletName',)
-    search_fields = ('outletName',)
-    list_filter = ('outletName',)
+    search_fields = ('outletName', 'inn', 'legalName')
+    list_filter = ('status', 'warehouse')
 
 
 @admin.register(Warehouse)
 class WarehouseAdmin(admin.ModelAdmin):
-    list_display = ('warehouseExternalCode', 'customerExternalCode',
-                    'warehouseName', 'allowTareReturn')
+    list_display = ('warehouseExternalCode', 'warehouseName',
+                    'customerExternalCode', 'allowTareReturn')
     list_editable = ('warehouseName',)
     search_fields = ('warehouseName',)
-    list_filter = ('warehouseName',)
+    #list_filter = ('warehouseName',)
 
 
 @admin.register(ProductStock)
@@ -42,4 +43,4 @@ class ProductStockAdmin(admin.ModelAdmin):
     list_display = ('warehouse', 'product', 'stock')
     list_editable = ('stock',)
     search_fields = ('warehouse', 'product')
-    list_filter = ('warehouse', 'product')
+    list_filter = ('warehouse',)
